43e03ebc7fd49f7a058d33b9e7c5f4a9b27df4bb
NilaySangle <77836660+NilaySangle@users.noreply.github.com>
2021-09-29 13:55:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var ball;
+var hypnoticBall;
+var database;
+var position;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var ballPosition = database.ref("ball/position");
+    ballPosition.on("value",readPosition,showError);
+}
+
+function draw(){
+    background("white");
+    if(position!==undefined){
+
+    
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-2,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(2,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-2);   
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+2);
+    }
+    drawSprites();
+}
+}
+
+function writePosition(x,y){
+    database.ref("ball/position").set({
+        "x":position.x+x,
+        "y":position.y+y
+    })
+}
+
+function readPosition(data){
+
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+
+}
+
+function showError(){
+
+    console.log("Firebase has an error finding your data");
+}
